9eb2a30b75ef9fb0114204b47ec2f4e4e1f61a62
he.wenyao <he.wenyao@qq.com>
2023-03-23 22:41:42 +0800

    增加 52bqg 爬取

---
+++ xiaoshuo/main.py
@@ -0,0 +1,4 @@
+import w52bqg
+
+if __name__ == '__main__':
+    w52bqg.spider_book("book_1004")
---
+++ xiaoshuo/w52bqg.py
@@ -0,0 +1,69 @@
+import requests
+import logging
+
+
+def __get_article_info(url, title, book_name):
+    """
+    :param url: 文章链接
+    :param title: 文章标题
+    :return:
+    """
+    response = requests.get(url)
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    content = soup.find('div', id='content')
+    content_text = str(content)
+
+    content_text = content_text.replace('<br/><br/>', '\n')
+    content_text = content_text.replace('<br/>', '\n')
+
+    # 创建HTML解析器
+    from html.parser import HTMLParser
+
+    class MyHTMLParser(HTMLParser):
+        def __init__(self):
+            super().__init__()
+            self.text = ""
+
+        def handle_data(self, data):
+            self.text += data
+
+    # 解析HTML
+    parser = MyHTMLParser()
+    parser.feed(content_text)
+    content_text = parser.text
+    content_text = '\n'.join([line.lstrip() for line in content_text.split('\n')])
+    content_text = '\n'.join([line.lstrip() for line in content_text.split('\n') if 'www.w52bqg.org' not in line])
+
+    # 将结果写入文件
+    with open(f'{book_name}.txt', 'a', encoding='utf-8') as f:
+        f.write(title + "\n")
+        f.write(content_text)
+    print("%s 抓取成功" % title)
+
+
+def spider_book(book_id):
+    """
+    抓取指定 book_id 的书籍 \n
+    书籍链接 https://www.52bqg.org/book_19589/
+    :param book_id:  book_19589
+    :return:
+    """
+    baseURL = f"https://www.52bqg.org/{book_id}/"
+    response = requests.get(baseURL)
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    info = soup.find("div", id="info")
+    book_name = info.h1.text
+
+    dl_children = soup.find('dl').children
+    flag = False
+    for child in dl_children:
+        if child.name == "center" or flag:
+            flag = True
+            a = child.a
+            if a is not None and a.name == "a":
+                __get_article_info(baseURL + a["href"], a.text, book_name)
